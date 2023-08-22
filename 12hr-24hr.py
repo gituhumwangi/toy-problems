17c37897b2d5a2f5442fb00a18f98fa1956caff7
dennis <dennis.mwangi@student.moringaschool.com>
2023-08-22 15:23:12 +0300

    Functioning 12hr to 24 hr

--- 12hr-24hr.py
+++ 12hr-24hr.py
@@ -1,4 +1,11 @@
 def time_conversion (hrs, mins, period) :
-    hrs = range(1, 13)
-    mins 
-    return
+    if hrs <= 12 and mins <=  59 and period == "am"  :
+        hr24 = str(hrs) + str(mins) + "hrs"
+        return hr24
+    elif hrs <= 12 and mins <= 59 and period == "pm" :
+        hrs24 = hrs + 12 
+        hr24 =str(hrs24) + str(mins) + "hrs"
+        return hr24
+    
+
+        
